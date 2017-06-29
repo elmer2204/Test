745e534d800a91c9c046a940781c2ce423cd38f5
elmer2204 <moragaelmer09@gmail.com>
2017-06-29 06:02:22 -0600

    mi segundo commit

--- src/IU.java
+++ src/IU.java
@@ -4,6 +4,7 @@ public class IU {
 	public static void main(String[] args) {
 		Persona persona=new Persona("Elmer","Moraga",20);
 		System.out.println(persona.ToString());
+		System.out.println("Soy el mejor");
 	}
 
 }
